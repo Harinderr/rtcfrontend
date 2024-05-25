c45b320571a77f4bbcf7562c320ee4071cd1dee3
Harinder <nexttoeveryone428@gmail.com>
2024-05-25 17:02:48 +0530

    url fixed

--- src/app/user/chat/page.jsx
+++ src/app/user/chat/page.jsx
@@ -45,7 +45,7 @@ export default function Chat() {
 
   //websocket
   function connnectToWs() {
-    const ws = new WebSocket("ws://localhost:5000");
+    const ws = new WebSocket("wss://witty-graceful-winterberry.glitch.me");
 
     setWs(ws);
 
--- src/utility/auth.js
+++ src/utility/auth.js
@@ -1,16 +1,35 @@
-import NextAuth from "next-auth"
+
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./prismaClient"
+
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  // Configure one or more authentication providers
+
+
   providers: [
    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
+
   ],
+  callbacks: {
+    async jwt(token, user, account, profile, isNewUser) {
+      // Add your JWT logic here
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session(session, token) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 }
 
